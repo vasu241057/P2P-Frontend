615ec4c6d5d192cfd11dc2cff8b3c78e44edc5b1
Vasu Khandelwal <f20190700@pilani.bits-pilani.ac.in>
2024-09-30 20:54:50 +0530

    sign-in fixed

--- src/routes/sign-in.tsx
+++ src/routes/sign-in.tsx
@@ -4,7 +4,7 @@ export default function SignInPage() {
   return (
     <div className="w-full h-screen flex items-center justify-center bg-black">
       <div className="max-w-md w-full p-6 bg-black rounded-lg shadow-md">
-        <SignIn path="/" signUpFallbackRedirectUrl="/sign-up" />
+        <SignIn signUpFallbackRedirectUrl="/sign-up" />
       </div>
     </div>
   );
